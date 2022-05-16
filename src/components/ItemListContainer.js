854cbca44782eefe76bd37e39f6084ee10d1e015
Maximiliano Agudin <maxiragudin@hotmail.com>
2022-05-16 18:35:48 -0300

    detalle del producto

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 import {prod} from '../productos';
 import ItemList from './ItemList'
+//import cocina1 from '../image/c1(1).jpg'
+//import dormi1 from '../image/d1.jpg'
+//import living1 from '../image/l1.jpg'
+
 
 //import ItemCount from 'ItemCount'
 
@@ -14,7 +18,7 @@ const ItemListContainer = () => {
        data.then ((data) => {
            setItems(data);
        });    
-       data.cach(err => { 
+       data.catch(err => { 
            console.log(err); 
        });
     }, []);
--- src/productos.js
+++ src/productos.js
@@ -1,20 +1,25 @@
+
+//import cocina1 from './image/c1(1).jpg'
+//import dormi1 from './image/d1.jpg'
+//import living1 from './image/l1.jpg'
+
 export const prod = [
     {   id: 1,
         nombre: 'prod 1',
         precio: 100,
         stock: 30,   
-        imagen: 'https://via.placeholder.com/150'
+        imagen: './image/c1(1).jpg'
     },
     {   id: 1,
         nombre: 'prod 2',
         precio: 100,
         stock: 30,   
-        imagen: 'https://via.placeholder.com/150'
+        imagen: './image/d1.jpg'
     },
     {   id: 1,
         nombre: 'prod 3',
         precio: 100,
         stock: 30,   
-        imagen: 'https://via.placeholder.com/150'
+        imagen: './image/l1.jpg'
     },
 ];
